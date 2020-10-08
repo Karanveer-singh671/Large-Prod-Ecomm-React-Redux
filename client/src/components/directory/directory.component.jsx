d6e0237d9dab41befff49826c42f045cd1aca6a8
Karanveer Singh <singhkaranveer671@gmail.com>
2020-10-08 13:22:56 -0700

    images, titles etc. added to sections state in directory component

--- client/src/components/directory/directory.component.jsx
+++ client/src/components/directory/directory.component.jsx
@@ -5,13 +5,35 @@ import "./directory.styles.scss";
 class Directory extends React.Component {
 	constructor() {
 		super();
+
 		this.state = {
 			sections: [
 				{
-					title: "content",
-					imageUrl: "#",
+					title: "hats",
+					imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+					id: 1,
+				},
+				{
+					title: "jackets",
+					imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+					id: 2,
+				},
+				{
+					title: "sneakers",
+					imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+					id: 3,
+				},
+				{
+					title: "womens",
+					imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
 					size: "large",
-					id: "1",
+					id: 4,
+				},
+				{
+					title: "mens",
+					imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+					size: "large",
+					id: 5,
 				},
 			],
 		};

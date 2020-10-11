b92994f6d167dd9d2c6c8c2b05ae7f20e3f31dbc
Karanveer Singh <singhkaranveer671@gmail.com>
2020-10-11 10:26:29 -0700

    styling for custom button hover etc.

--- client/src/components/custom-button/custom-button.component.jsx
+++ client/src/components/custom-button/custom-button.component.jsx
@@ -2,11 +2,18 @@ import React from "react";
 
 import "./custom-button.styles.scss";
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+const CustomButton = ({
+	children,
+	isGoogleSignIn,
+	inverted,
+	...otherProps
+}) => (
 	// this way will pass all the properties from parent component where call CustomButton into button tag
 	// children will be whatever put between opening and closing tag from where this component is called
 	<button
-		className={`${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
+		className={`${inverted ? "inverted" : ""} ${
+			isGoogleSignIn ? "google-sign-in" : ""
+		} custom-button`}
 		{...otherProps}
 	>
 		{children}

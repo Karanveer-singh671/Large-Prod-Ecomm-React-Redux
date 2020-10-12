4c6b86de86c7ec5dbc8c66c84188ef96e7693991
Karanveer Singh <singhkaranveer671@gmail.com>
2020-10-12 10:15:13 -0700

    curried selector to grab url param and map it to number id in array and added route and ownProps so could have access to match in mapStateToProps

--- client/src/pages/collection/collection.component.jsx
+++ client/src/pages/collection/collection.component.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { connect } from "react-redux";
+import { selectCollection } from "../../redux/shop/shop.selectors";
 import CollectionItem from "../../components/collection-item/collection-item.component";
 import "./collection.styles.scss";
 
-const CollectionPage = ({ match }) => (
+const CollectionPage = ({ collection }) => (
 	<div className="collection-page">
 		<h2>Collection Page</h2>
 	</div>
 );
-export default CollectionPage;
+// ownProps includes all props in our component that is connected using connect gives access to match, history etc
+const mapStateToProps = (state, ownProps) => ({
+	// this needs the state since selectCollection needs a part of the state depending on the URL parameter (takes in collectionUrlParam as a paramter so need to pass state)
+	collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+export default connect(mapStateToProps)(CollectionPage);
--- client/src/redux/shop/shop.selectors.js
+++ client/src/redux/shop/shop.selectors.js
@@ -1,8 +1,30 @@
+// reselect memoizes our selectors
 import { createSelector } from "reselect";
+// memoize is same idea as reselect for our selectors except memoizing the return of our function which returns a selector 
+import memoize from "lodash.memoize";
+// need since routes go to /hats / sneakers etc. but the id we get is a number so need to map
+const COLLECTION_ID_MAP = {
+	hats: 1,
+	sneakers: 2,
+	jackets: 3,
+	womens: 4,
+	mens: 5,
+};
 
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
 	[selectShop],
 	(shop) => shop.collections
+);
+// wrap in memoize, whenever this function gets called and receieves collectionUrlParam memoizes return of the function
+// in this case returning a selector, if function gets called again w/ same collectionUrlParam don't rerun the function
+// just return selector that's been stored
+export const selectCollection = memoize((collectionUrlParam) =>
+	createSelector([selectCollections], (collections) =>
+		collections.find(
+			// find collection.id matching the url parameter of our collectionIDMAP
+			(collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+		)
+	)
 );

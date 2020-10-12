bceca43c3caa457eafbe7e998fa956e6435fc37c
Karanveer Singh <singhkaranveer671@gmail.com>
2020-10-12 10:33:24 -0700

    change shop data to object, this way can search thru in O(1) time instead of O(n) for array find, add key to object as same as title propery (url) and modify selector

--- client/src/pages/collection/collection.component.jsx
+++ client/src/pages/collection/collection.component.jsx
@@ -4,11 +4,20 @@ import { selectCollection } from "../../redux/shop/shop.selectors";
 import CollectionItem from "../../components/collection-item/collection-item.component";
 import "./collection.styles.scss";
 
-const CollectionPage = ({ collection }) => (
-	<div className="collection-page">
-		<h2>Collection Page</h2>
-	</div>
-);
+const CollectionPage = ({ collection }) => {
+	const { title, items } = collection;
+	return (
+		<div className="collection-page">
+			<h2 className="title">{title}</h2>
+			<div className="items">
+				{items.map((item) => (
+					<CollectionItem key={item.id} item={item} />
+				))}
+			</div>
+		</div>
+	);
+};
+
 // ownProps includes all props in our component that is connected using connect gives access to match, history etc
 const mapStateToProps = (state, ownProps) => ({
 	// this needs the state since selectCollection needs a part of the state depending on the URL parameter (takes in collectionUrlParam as a paramter so need to pass state)

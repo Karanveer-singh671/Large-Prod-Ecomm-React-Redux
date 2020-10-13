3e16e53a7eb0bbe5c2435d4ff4407d7be908daff
Karanveer Singh <singhkaranveer671@gmail.com>
2020-10-13 14:50:11 -0700

    fireabase util change and spinner comment

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+
 const config = {
 	apiKey: "AIzaSyASiiEHmY9aRyrcEBYwfe3RSf19D71fd-4",
 	authDomain: "e-commerce-react-redux.firebaseapp.com",
@@ -11,10 +12,11 @@ const config = {
 	appId: "1:509034361563:web:aaf3f107c0e07f0c32d2fe",
 	measurementId: "G-CC5Z50NX5P",
 };
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-	if (!userAuth) {
-		return;
-	}
+	if (!userAuth) return;
+
 	const userRef = firestore.doc(`users/${userAuth.uid}`);
 
 	const snapShot = await userRef.get();
@@ -22,7 +24,6 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 	if (!snapShot.exists) {
 		const { displayName, email } = userAuth;
 		const createdAt = new Date();
-
 		try {
 			await userRef.set({
 				displayName,
@@ -30,21 +31,34 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 				createdAt,
 				...additionalData,
 			});
-		} catch (err) {
-			console.log("error creating user", err.message);
+		} catch (error) {
+			console.log("error creating user", error.message);
 		}
 	}
+
 	return userRef;
 };
 
-firebase.initializeApp(config);
+export const addCollectionAndDocuments = async (
+	collectionKey,
+	objectsToAdd
+) => {
+	const collectionRef = firestore.collection(collectionKey);
+
+	const batch = firestore.batch();
+	objectsToAdd.forEach((obj) => {
+		const newDocRef = collectionRef.doc();
+		batch.set(newDocRef, obj);
+	});
+
+	return await batch.commit();
+};
 
 export const convertCollectionsSnapshotToMap = (collections) => {
 	const transformedCollection = collections.docs.map((doc) => {
 		const { title, items } = doc.data();
 
 		return {
-			// converts character that url cannot understand to
 			routeName: encodeURI(title.toLowerCase()),
 			id: doc.id,
 			title,
@@ -53,30 +67,11 @@ export const convertCollectionsSnapshotToMap = (collections) => {
 	});
 
 	return transformedCollection.reduce((accumulator, collection) => {
-		// each index value is going to be title key and sets that key's value to collection
-		// make sure accumulator returned then will return object of all keys title and values for each of them
-		accumulator[collection.title.toLowerCase()] = collection
-		return accumulator
-	}, {})
+		accumulator[collection.title.toLowerCase()] = collection;
+		return accumulator;
+	}, {});
 };
 
-export const addCollectionAndDocuments = async (
-	collectionKey,
-	objectsToAdd
-) => {
-	const collectionRef = firestore.collection(collectionKey);
-	// create a batch so if 1 record fails to set the whole operation fails (predictability)
-	const batch = firestore.batch();
-	objectsToAdd.forEach((obj) => {
-		// make new Document reference object and generate random id
-		// if added obj.title would have the titles in our shop data
-		const newDocRef = collectionRef.doc();
-		// set the batch
-		batch.set(newDocRef, obj);
-	});
-	// commit the batch (returns a promise)
-	await batch.commit();
-};
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 

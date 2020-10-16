b8d845d55f4596e58ea4f959f265ee061347c9ec
Karanveer Singh <singhkaranveer671@gmail.com>
2020-10-16 10:20:24 -0700

    removed / route

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.get("/", (req, res) => res.send("Hello World!"));
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "client/build")));
 	app.get("*", (req, res) =>

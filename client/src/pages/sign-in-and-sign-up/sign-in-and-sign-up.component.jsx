aac31c971b48cfefb6a8e31a05f1723fc7c65fc6
Karanveer Singh <singhkaranveer671@gmail.com>
2020-10-18 15:08:15 -0700

    signinandsignup page media query

--- client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ client/src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-import "./sign-in-and-sign-up.styles.scss";
+import "./sign-in-and-sign-up.jsx";
 import Signin from "../../components/signin/signin.component";
-import SignUp from "../../components/signup/signup.component"
+import SignUp from "../../components/signup/signup.component";
 
 const SignInAndSignUpPage = () => (
 	<div className="sign-in-and-sign-up">

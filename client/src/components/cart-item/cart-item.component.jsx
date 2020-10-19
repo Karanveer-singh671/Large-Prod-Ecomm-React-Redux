b934705469497bf989781287036aaf1f2f338049
Karanveer Singh <singhkaranveer671@gmail.com>
2020-10-19 10:20:38 -0700

    memo

--- client/src/components/cart-item/cart-item.component.jsx
+++ client/src/components/cart-item/cart-item.component.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
+import React from "react";
 
 import {
-  CartItemContainer,
-  ItemDetailsContainer,
-  CartItemImage
-} from './cart-item.styles';
+	CartItemContainer,
+	ItemDetailsContainer,
+	CartItemImage,
+} from "./cart-item.styles";
 
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
-  <CartItemContainer>
-    <CartItemImage src={imageUrl} alt='item' />
-    <ItemDetailsContainer>
-      <span>{name}</span>
-      <span>
-        {quantity} x ${price}
-      </span>
-    </ItemDetailsContainer>
-  </CartItemContainer>
+	<CartItemContainer>
+		<CartItemImage src={imageUrl} alt="item" />
+		<ItemDetailsContainer>
+			<span>{name}</span>
+			<span>
+				{quantity} x ${price}
+			</span>
+		</ItemDetailsContainer>
+	</CartItemContainer>
 );
-
-export default CartItem;
+// add react.memo since each item in the dropdown is re-rendered when adding a completely separate item
+export default React.memo(CartItem);
